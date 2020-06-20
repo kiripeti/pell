52586ee5c5309d9e503b50f86c8542c5a0797065
EUROPE\hunkip <peter.kiraly@sas.com>
2020-06-20 15:26:46 +0200

    make install verbose

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ HTDOC=/sas/decman/Lev1/Web/WebServer/htdocs
 curl https://codeload.github.com/kiripeti/pell/zip/master > pell-master.zip
 unzip pell-master.zip
 
-cp pell-master/SAS/SASMacro/init_stp.sas ${SASE}/SASMacro/init_stp.sas
-cp pell-master/SAS/SASMacro/preprocess.sas ${SASE}/SASMacro/preprocess.sas
-cp pell-master/SAS/STP/calculateBenefits.sas ${SASE}/SASCode/STP/calculateBenefits.sas
-cp pell-master/SAS/STP/getBenefits.sas ${SASE}/SASCode/STP/getBenefits.sas
-cp pell-master/SAS/STP/getCustomer.sas ${SASE}/SASCode/STP/getCustomer.sas
+cp -v pell-master/SAS/SASMacro/init_stp.sas ${SASE}/SASMacro/init_stp.sas
+cp -v pell-master/SAS/SASMacro/preprocess.sas ${SASE}/SASMacro/preprocess.sas
+cp -v pell-master/SAS/STP/calculateBenefits.sas ${SASE}/SASCode/STP/calculateBenefits.sas
+cp -v pell-master/SAS/STP/getBenefits.sas ${SASE}/SASCode/STP/getBenefits.sas
+cp -v pell-master/SAS/STP/getCustomer.sas ${SASE}/SASCode/STP/getCustomer.sas
 
 rm -R ${HTDOC}/pell-react
-cp -R pell-master/build ${HTDOC}/pell-react
+cp -Rv pell-master/build ${HTDOC}/pell-react
 
 rm -R pell-master
 rm pell-master.zip
