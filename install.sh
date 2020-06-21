1ea2655b120797810b31a5af41ad12be3ee40229
EUROPE\hunkip <peter.kiraly@sas.com>
2020-06-21 12:51:49 +0200

    install branch or tag

--- install.sh
+++ install.sh
@@ -2,15 +2,16 @@
 
 SASE=/sas/decman/Lev1/SASApp/SASEnvironment
 HTDOC=/sas/decman/Lev1/Web/WebServer/htdocs
+TAG=${1}
 
-curl https://codeload.github.com/kiripeti/pell/zip/master > pell-master.zip
-unzip pell-master.zip
+curl https://codeload.github.com/kiripeti/pell/zip/${TAG} > pell-${TAG}.zip
+unzip pell-${TAG}.zip
 
-cp -Rv pell-master/SAS/SASMacro ${SASE}
-cp -Rv pell-master/SAS/STP ${SASE}/SASCode
+cp -Rv pell-${TAG}/SAS/SASMacro ${SASE}
+cp -Rv pell-${TAG}/SAS/STP ${SASE}/SASCode
 
 rm -R ${HTDOC}/pell-react
-cp -Rv pell-master/build ${HTDOC}/pell-react
+cp -Rv pell-${TAG}/build ${HTDOC}/pell-react
 
-rm -R pell-master
-rm pell-master.zip
+rm -R pell-${TAG}
+rm pell-${TAG}.zip
