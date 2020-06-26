94245b93663964d0835b5b32b5c8663e22a14ff5
EUROPE\hunkip <peter.kiraly@sas.com>
2020-06-26 09:22:19 +0200

    build 2.0.1

---
+++ build/precache-manifest.6491dfb6f6bcc198908bdb120500febf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80df1c1cbb44fe82493c713f5c590a5b",
+    "url": "/pell-react/index.html"
+  },
+  {
+    "revision": "6bcc027996313222551c",
+    "url": "/pell-react/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "8ef06036f67ee27f34fb",
+    "url": "/pell-react/static/css/main.3165873f.chunk.css"
+  },
+  {
+    "revision": "6bcc027996313222551c",
+    "url": "/pell-react/static/js/2.8b780ff1.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/pell-react/static/js/2.8b780ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef06036f67ee27f34fb",
+    "url": "/pell-react/static/js/main.d42b04c9.chunk.js"
+  },
+  {
+    "revision": "ab42450af6285eddc032",
+    "url": "/pell-react/static/js/runtime-main.346265af.js"
+  }
+]);
